ef2e98c92ccf39dbb69310f7238ebd752b34079f
Shashank Kumar <abc@shanky.xyz>
2017-01-09 01:14:49 +0530

    Website is now mobile compatible

--- js/master.js
+++ js/master.js
@@ -38,4 +38,65 @@ $(document).ready(function(){
       });
     }
   });
+
+  $("#nav_button").click(function(){
+    if($("#nav_button").hasClass("nav_button_click"))
+    {
+      $("#nav_button").removeClass("nav_button_click");
+      $(".nav_mobile").fadeOut("fast");
+    }
+    else {
+      $("#nav_button").addClass("nav_button_click");
+      $(".nav_mobile").fadeIn("fast");
+    }
+  });
+
+  $("#about_button2").click(function(){
+    $("#nav_button").removeClass("nav_button_click");
+    $(".nav_mobile").fadeOut("fast", function() {
+      if($(".past_meetup").css("display")=="block")
+      {
+        $(".past_meetup").fadeOut("fast",function(){
+          $(".about").fadeIn("fast");
+        });
+      }
+      else {
+        $(".contact").fadeOut("fast",function(){
+          $(".about").fadeIn("fast");
+        });
+      }
+    });
+  });
+  $("#past_meetup_button2").click(function(){
+    $("#nav_button").removeClass("nav_button_click");
+    $(".nav_mobile").fadeOut("fast", function(){
+      if($(".contact").css("display")=="block")
+      {
+        $(".contact").fadeOut("fast",function(){
+          $(".past_meetup").fadeIn("fast");
+        });
+      }
+      else {
+        $(".about").fadeOut("fast",function(){
+          $(".past_meetup").fadeIn("fast");
+        });
+      }
+    });
+  });
+  $("#contact_button2").click(function(){
+    $("#nav_button").removeClass("nav_button_click");
+    $(".nav_mobile").fadeOut("fast", function(){
+      if($(".past_meetup").css("display")=="block")
+      {
+        $(".past_meetup").fadeOut("fast",function(){
+          $(".contact").fadeIn("fast");
+        });
+      }
+      else {
+        $(".about").fadeOut("fast",function(){
+          $(".contact").fadeIn("fast");
+        });
+      }
+    });
+  });
 });
